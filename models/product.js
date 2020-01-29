888faf156aabb12904174f485db946841c649c76
lisun <oracle@lisun.me>
2020-01-29 03:35:58 +0000

    added /models

---
+++ models/product.js
@@ -0,0 +1,19 @@
+const products = [];
+
+module.exports = class Product {
+    constructor(t){
+        this.title = t;
+
+    }
+
+    save() {
+        products.push(this);
+    }
+
+    static fetchAll(){
+        return this.products;
+    }
+
+
+
+}
