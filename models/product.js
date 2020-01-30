0343f65c170978d114a2031869507e1c0b02a480
EC2 Default User <ec2-user@ip-10-0-1-77.ec2.internal>
2020-01-30 04:56:15 +0000

    finished mvc

--- models/product.js
+++ models/product.js
@@ -1,19 +1,31 @@
-const products = [];
-
+const fs = require("fs");
+const path = require('path');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
 module.exports = class Product {
-    constructor(t){
-        this.title = t;
+  constructor(t) {
+    this.title = t;
+  }
 
-    }
+  save() {
+    
+    fs.readFile(p, (err, fileContent)=> {
+      let products = [];
+      if(!err){
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err)=>{
+        console.log(err)
+      });
+    });
+  }
 
-    save() {
-        products.push(this);
-    }
-
-    static fetchAll(){
-        return this.products;
-    }
-
-
-
-}
+  static fetchAll(cb) {
+    this.products = JSON.parse(fs.readFileSync(p));
+    return this.products;
+  }
+};
